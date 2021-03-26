5eb5b973581030591aa9dcfd5a429791ef38c752
Aaron T <aronthakur@gmail.com>
2021-03-25 19:10:31 -0700

    hdh

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import styles from './Navbar.module.css'
+
+export default function Navbar(props: any) {
+    return (
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light ">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <p>Hi</p>
+            </nav>
+        </div>
+    );
+}
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,5 +1,72 @@
+import './AboutPage.css'
+import Navbar from '../../components/Navbar/Navbar'
+
 export default function AboutPage(props: any) {
     return (
-        <div>Es about page</div>
+        <div>
+            <Navbar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laborLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div>    
+                    </div>
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div> 
+                    </div>
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div> 
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div>    
+                    </div>
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div> 
+                    </div>
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div> 
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div>    
+                    </div>
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div> 
+                    </div>
+                    <div className="col-sm text-left">
+                        <div>
+                            <h1 className="font-weight-bold">Lorem Ipsum?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>       
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
