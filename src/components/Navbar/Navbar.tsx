38a9aa060f1dc565d23e747839aa962f49a54cec
Aaron T <aronthakur@gmail.com>
2021-03-26 13:13:55 -0700

    dj

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,36 @@
-import styles from './Navbar.module.css'
+import './Navbar.css'
 
 export default function Navbar(props: any) {
     return (
-        <div className="container">
-            <nav className="navbar navbar-expand-lg navbar-light ">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <p>Hi</p>
-            </nav>
+        <div>
+        <div className="myNav navbar-overlay">
+          <a href="javascript:void(0)" className="navbar-closebtn">&times;</a>
+          <div className="navbar-overlay-content">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+          </div>
+        </div>
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">DONVERSITY</a>
+            <span>
+              <div className="nav-icon4">
+              <span></span>
+              <span></span>
+              <span></span>
+              </div>
+            </span>
+        </nav>
+        <div className = "hi"></div> 
         </div>
     );
 }
