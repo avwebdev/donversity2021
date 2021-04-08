7373e6e461f9a2268cee45a2342a89c8b6b90ab4
Yash Totale <totaleyash@gmail.com>
2021-04-08 11:42:34 -0700

    Added Firebase and Redux

---
+++ src/misc/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAnG9JvELKD2EYdfRFavSNsJEo2qhmLTwc",
+  authDomain: "donversity-2021.firebaseapp.com",
+  projectId: "donversity-2021",
+  storageBucket: "donversity-2021.appspot.com",
+  messagingSenderId: "336974977243",
+  appId: "1:336974977243:web:db6d0a53f0e209a7f2d4fe",
+  measurementId: "G-44NJM0LKVV",
+};
+
+export default firebaseConfig;
