597b85bcc422b8536839e99307397d30c1d07043
Yash Totale <totaleyash@gmail.com>
2021-04-15 11:03:37 -0700

    Added Individual Club Pages

---
+++ src/pages/ClubPage/ClubPage.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { DataContext } from "../../misc/DataContext";
+import { ContentSections } from "../../types";
+import styles from "./ClubPage.module.css";
+
+interface Params {
+  club: string;
+}
+
+export default function ClubPage() {
+  const data = useContext(DataContext) as ContentSections;
+  const { club } = useParams<Params>();
+
+  const clubInfo = data.clubs.find((c) => c.id === club);
+
+  if (!clubInfo) return <Redirect to="/club-fair" />;
+
+  return (
+    <div className={styles.club}>
+      <h1>{clubInfo.name}</h1>
+      {clubInfo.description.split("\n").map((fragment, i) => (
+        <h6 key={i} className={styles.clubDescription}>
+          {fragment}
+        </h6>
+      ))}
+    </div>
+  );
+}
--- src/pages/ClubsPage/ClubsPage.tsx
+++ src/pages/ClubsPage/ClubsPage.tsx
@@ -2,28 +2,31 @@ import React, { useContext, useState } from "react";
 import styles from "./ClubsPage.module.css";
 import { DataContext } from "../../misc/DataContext";
 import { ContentSections, Club } from "../../types";
+import { Link } from "react-router-dom";
 
 function ClubDiv(club: Club) {
   const [hovering, setHovering] = useState(false);
 
   return (
-    <div
-      className={styles.club}
-      onMouseOver={() => setHovering(true)}
-      onMouseLeave={() => setHovering(false)}
-    >
+    <Link to={`/club-fair/${club.id}`}>
       <div
-        className={`${styles.overlay} ${hovering && styles.overlayShow}`}
-      ></div>
-      <img
-        src={`/clubs/${club.image}`}
-        alt={club.name}
-        className={styles.clubImage}
-      />
-      <h5 className={`${styles.clubName} ${hovering && styles.clubNameShow}`}>
-        {club.name}
-      </h5>
-    </div>
+        className={styles.club}
+        onMouseOver={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+      >
+        <div
+          className={`${styles.overlay} ${hovering && styles.overlayShow}`}
+        ></div>
+        <img
+          src={`/clubs/${club.image}`}
+          alt={club.name}
+          className={styles.clubImage}
+        />
+        <h5 className={`${styles.clubName} ${hovering && styles.clubNameShow}`}>
+          {club.name}
+        </h5>
+      </div>
+    </Link>
   );
 }
 
