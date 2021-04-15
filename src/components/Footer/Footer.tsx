8777c3a12c944154873f45f57e64c47dcb1c8ee1
Yash Totale <totaleyash@gmail.com>
2021-04-15 15:00:56 -0700

    Changed Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-light d-flex justify-content-center">
-      Created by the AV Web Team (
-      <a href="https://github.com/YashTotale">Yash Totale</a>&nbsp;and&nbsp;
-      <a href="https://www.linkedin.com/in/subha-das-760b861aa/">Subha Das</a>)
+      Created by the AV Web Team
     </footer>
   );
 }
