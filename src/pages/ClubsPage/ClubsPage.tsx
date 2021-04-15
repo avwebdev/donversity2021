850d8f10126222c40c98ca3bcc89c5adb0db1765
Yash Totale <totaleyash@gmail.com>
2021-04-15 00:01:43 -0700

    Initial Clubs Page

--- src/pages/ClubsPage/ClubsPage.tsx
+++ src/pages/ClubsPage/ClubsPage.tsx
@@ -1,11 +1,40 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useState } from "react";
+import styles from "./ClubsPage.module.css";
 import { DataContext } from "../../misc/DataContext";
-import { ContentSections } from "../../types";
+import { ContentSections, Club } from "../../types";
+
+function ClubDiv(club: Club) {
+  const [hovering, setHovering] = useState(false);
+
+  return (
+    <div
+      className={styles.club}
+      onMouseOver={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
+    >
+      <div
+        className={`${styles.overlay} ${hovering && styles.overlayShow}`}
+      ></div>
+      <img
+        src={`/clubs/${club.image}`}
+        alt={club.name}
+        className={styles.clubImage}
+      />
+    </div>
+  );
+}
 
 export default function ClubsPage() {
   const data = useContext(DataContext) as ContentSections;
-  console.log(data);
 
-  return null;
+  return (
+    <>
+      <h1 className={styles.heading}>Club Fair!</h1>
+      <div className={styles.clubs}>
+        {data.clubs.map((club, i) => (
+          <ClubDiv key={i} {...club} />
+        ))}
+      </div>
+    </>
+  );
 }
