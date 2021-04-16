0aed9ff3a76c0ae6234a7204d8bdd26330bc671e
Yash Totale <totaleyash@gmail.com>
2021-04-16 07:59:53 -0700

    Fixed Club Carousel

--- src/pages/ClubPage/ClubPage.tsx
+++ src/pages/ClubPage/ClubPage.tsx
@@ -4,8 +4,8 @@ import MediaView from "../../components/MediaView/MediaView";
 import { DataContext } from "../../misc/DataContext";
 import { ContentSections } from "../../types";
 import styles from "./ClubPage.module.css";
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import "react-alice-carousel/lib/scss/alice-carousel.scss";
 import { Link } from "react-router-dom";
 
@@ -14,210 +14,10 @@ interface Params {
 }
 
 export default function ClubPage() {
-
-  
- 
   const data = useContext(DataContext) as ContentSections;
   const { club } = useParams<Params>();
 
   const clubInfo = data.clubs.find((c) => c.id === club);
-  console.log(data.clubs);
-
- 
-  const style = {width:400, paddingRight:20};
-
-
-  
-    const items = [
-
-      
-      <div style={style}>
-      <Link className="test" to={`/club-fair/${data.clubs[0].id}`}>
-        <div className={`${styles.overlay} ${styles.overlayShow}`}>
-          <h4
-            className={`${styles.clubName} ${styles.clubNameShow}`}
-          >
-            {data.clubs[0].name}
-          </h4>
-        </div>
-        <img
-          src={`/clubs/${data.clubs[0].image}`}
-          alt={data.clubs[0].name}
-          className={styles.clubImage}/>
-          </Link>
-      </div>,
-     
-      <div style={style}>
-      <Link to={`/club-fair/${data.clubs[1].id}`}>
-      <div className={`${styles.overlay} ${styles.overlayShow}`}>
-        <h4
-          className={`${styles.clubName} ${styles.clubNameShow}`}
-        >
-          {data.clubs[1].name}
-        </h4>
-      </div>
-      <img
-        src={`/clubs/${data.clubs[1].image}`}
-        alt={data.clubs[1].name}
-        className={styles.clubImage}/>
-        </Link>
-    </div>,
-   
-    <div style={style}>
-    <Link to={`/club-fair/${data.clubs[2].id}`}>
-    <div className={`${styles.overlay} ${styles.overlayShow}`}>
-      <h4
-        className={`${styles.clubName} ${styles.clubNameShow}`}
-      >
-        {data.clubs[2].name}
-      </h4>
-    </div>
-    <img
-      src={`/clubs/${data.clubs[2].image}`}
-      alt={data.clubs[2].name}
-      className={styles.clubImage}/>
-      </Link>
-  </div>,
-  
-  <div style={style}>
- <Link to={`/club-fair/${data.clubs[3].id}`}>
-  <div className={`${styles.overlay} ${styles.overlayShow}`}>
-    <h4
-      className={`${styles.clubName} ${styles.clubNameShow}`}
-    >
-      {data.clubs[3].name}
-    </h4>
-  </div>
-  <img
-    src={`/clubs/${data.clubs[3].image}`}
-    alt={data.clubs[3].name}
-    className={styles.clubImage}/>
-    </Link>
-</div>,
-//************************************************** */
-
-<div style={style}>
-<Link to={`/club-fair/${data.clubs[4].id}`}>
-  <div className={`${styles.overlay} ${styles.overlayShow}`}>
-    <h4
-      className={`${styles.clubName} ${styles.clubNameShow}`}
-    >
-      {data.clubs[4].name}
-    </h4>
-  </div>
-  <img
-    src={`/clubs/${data.clubs[4].image}`}
-    alt={data.clubs[4].name}
-    className={styles.clubImage}/>
-    </Link>
-</div>,
-//************************************************** */
-
-<div  style={style}>
-<Link to={`/club-fair/${data.clubs[5].id}`}>
-  <div className={`${styles.overlay} ${styles.overlayShow}`}>
-    <h4
-      className={`${styles.clubName} ${styles.clubNameShow}`}
-    >
-      {data.clubs[5].name}
-    </h4>
-  </div>
-  <img
-    src={`/clubs/${data.clubs[5].image}`}
-    alt={data.clubs[5].name}
-    className={styles.clubImage}/>
-    </Link>
-</div>,
-//************************************************** */
-
-<div  style={style}>
-<Link to={`/club-fair/${data.clubs[6].id}`}>
-  <div className={`${styles.overlay} ${styles.overlayShow}`}>
-    <h4
-      className={`${styles.clubName} ${styles.clubNameShow}`}
-    >
-      {data.clubs[6].name}
-    </h4>
-  </div>
-  <img
-    src={`/clubs/${data.clubs[6].image}`}
-    alt={data.clubs[6].name}
-    className={styles.clubImage}/>
-    </Link>
-</div>,
-//************************************************** */
-
-<div  style={style}>
-<Link to={`/club-fair/${data.clubs[7].id}`}>
-  <div className={`${styles.overlay} ${styles.overlayShow}`}>
-    <h4
-      className={`${styles.clubName} ${styles.clubNameShow}`}
-    >
-      {data.clubs[7].name}
-    </h4>
-  </div>
-  <img
-    src={`/clubs/${data.clubs[7].image}`}
-    alt={data.clubs[7].name}
-    className={styles.clubImage}/>
-    </Link>
-</div>,
-//************************************************** */
-
-<div   style={style}>
-<Link to={`/club-fair/${data.clubs[8].id}`}>
-  <div className={`${styles.overlay} ${styles.overlayShow}`}>
-    <h4
-      className={`${styles.clubName} ${styles.clubNameShow}`}
-    >
-      {data.clubs[8].name}
-    </h4>
-  </div>
-  <img
-    src={`/clubs/${data.clubs[8].image}`}
-    alt={data.clubs[8].name}
-    className={styles.clubImage}/>
-    </Link>
-</div>,
-//************************************************** */
-
-<div  style={style}>
-<Link to={`/club-fair/${data.clubs[9].id}`}>
-  <div className={`${styles.overlay} ${styles.overlayShow}`}>
-    <h4
-      className={`${styles.clubName} ${styles.clubNameShow}`}
-    >
-      {data.clubs[9].name}
-    </h4>
-  </div>
-  <img
-    src={`/clubs/${data.clubs[9].image}`}
-    alt={data.clubs[9].name}
-    className={styles.clubImage}/>
-    </Link>
-</div>,
-//************************************************** */
-
-<div style={style}>
-<Link to={`/club-fair/${data.clubs[10].id}`}>
-  <div className={`${styles.overlay} ${styles.overlayShow}`}>
-    <h4
-      className={`${styles.clubName} ${styles.clubNameShow}`}
-    >
-      {data.clubs[10].name}
-    </h4>
-  </div>
-  <img
-    src={`/clubs/${data.clubs[10].image}`}
-    alt={data.clubs[10].name}
-    className={styles.clubImage}/>
-    </Link>
-</div>,
-//************************************************** */
-
-    ];
-    
-   
 
   if (!clubInfo) return <Redirect to="/club-fair" />;
 
@@ -226,7 +26,7 @@ export default function ClubPage() {
       <h1>{clubInfo.name}</h1>
       <div className={styles.clubMedia}>
         <img
-          className={styles.clubimage}
+          className={styles.mainImage}
           src={`/clubs/${clubInfo.image}`}
           alt={clubInfo.name}
         />
@@ -243,11 +43,31 @@ export default function ClubPage() {
           {fragment}
         </p>
       ))}
-      <AliceCarousel mouseTracking items={items} autoWidth  
-        infinite  autoPlay
+      <AliceCarousel
+        mouseTracking
+        items={data.clubs
+          .filter((c) => c.id !== club)
+          .map((c, i) => (
+            <div key={i} className={styles.otherClub}>
+              <Link to={`/club-fair/${c.id}`}>
+                <div>
+                  <h4>{c.name}</h4>
+                </div>
+                <img
+                  src={`/clubs/${c.image}`}
+                  alt={c.name}
+                  className={styles.clubImage}
+                />
+              </Link>
+            </div>
+          ))}
+        autoWidth
+        infinite
+        autoPlay
         autoPlayInterval={2000}
         animationDuration={2000}
-        autoPlayStrategy="none"/>
+        autoPlayStrategy="none"
+      />
     </div>
   );
 }
