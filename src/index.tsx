4a07d55817628301707bb6781d4d6bd3d87a13a9
Yash Totale <totaleyash@gmail.com>
2021-04-19 13:32:33 -0700

    Dev branch redirects to main website

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import ReduxStore from "./Store";
+window.location.replace("https://donversity.amadorweb.org/");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ReduxStore>
-      <Router>
-        <App />
-      </Router>
-    </ReduxStore>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ReduxStore>
+//       <Router>
+//         <App />
+//       </Router>
+//     </ReduxStore>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+export {};
